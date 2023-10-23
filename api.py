3887d7495254892c9b2605e5c5df538fa12afbaf
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-23 08:33:15 -0400

    Update api.py

--- api.py
+++ api.py
@@ -3,3 +3,12 @@ import base64,requests,robloxapi
 from flask import Flask,jsonify,request
 #DEFINE APP
 app = Flask(__name__)
+#Define Some Functions
+def getRequest():
+    return request
+##end
+#Dynamic Pages
+@app.route('/')
+def index():
+    myQuery = getParams(request.url)
+##end
