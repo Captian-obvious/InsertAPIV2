8ed6a8141a598d64ec01d7f80ae5dbf060822d84
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-23 13:12:19 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,5 +3,10 @@ from api import app,getRequest
 ##class COMPILER:
 def parse(file):
     parsedString = ""
+    with open(file, "rb") as file:
+        root = BinaryRobloxFile()
+        root.deserialize(file)
+        print(root)
+    ##endwith
 ##end
 ##end
