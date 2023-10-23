0e7395251491883dd0df71b7583120617e8f481a
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-10-23 11:36:56 -0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -3,11 +3,5 @@ from api import app,getRequest
 ##class COMPILER:
 def parse(file):
     parsedString = ""
-    f = pyrbxm.read(file)
-    ##for object in f.Objects:
-    ##    if (isinstance(object)):
-    ##        parseInstance(object)
-        ##endif
-    ##end
 ##end
 ##end
