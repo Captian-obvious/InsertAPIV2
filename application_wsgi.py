8cccb7a380033dcd74fde28d0dfd1e92dbfcd98c
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-01 17:40:49 -0400

    Update application_wsgi.py

--- application_wsgi.py
+++ application_wsgi.py
@@ -13,4 +13,4 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 ##endif
 # import flask app but need to call it "application" for WSGI to work
-from main import app as application  # noqa
+from api import app as application  # noqa
