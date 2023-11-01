9318f4091f734be039e7aeb539cf4cb1e44a4a8e
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-01 17:29:12 -0400

    Update application_wsgi.py

--- application_wsgi.py
+++ application_wsgi.py
@@ -8,7 +8,7 @@
 import sys
 
 # add your project directory to the sys.path
-project_home = '/home/Captian-obvious/InsertAPIV2'
+project_home = '/opt/render/project/src'
 if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 ##endif
