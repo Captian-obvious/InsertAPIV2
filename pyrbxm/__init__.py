180aa5d495e66131114d4737cf9e085f39a1bfb1
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-03 07:03:45 -0400

    Update __init__.py

--- pyrbxm/__init__.py
+++ pyrbxm/__init__.py
@@ -1,6 +1,7 @@
 from .binary import *
 from .datatypes import *
 from .tree import *
+#idk anymore
 def readBinary(file):
     with open(file, "rb") as file:
         root = binary.BinaryRobloxFile()
