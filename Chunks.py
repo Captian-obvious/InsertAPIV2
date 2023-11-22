1c6dab380e275e68102382ed3beba2637d9a2b1c
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 18:36:11 -0500

    Update Chunks.py

--- Chunks.py
+++ Chunks.py
@@ -1,1 +1,16 @@
+import base64,Buffer,errorHandler,LZ4,os,pyrbxm,sys,requests,robloxapi
 
+def VirtualInstance(classID, className, ref):
+    class vi:
+        ClassId=classID
+		ClassName=className
+		Ref=ref
+		Properties={}
+		Children={}
+    ##end
+    return vi
+##end
+
+def INST(chunk, rbxm):
+    buf=chunk.Data
+##end
