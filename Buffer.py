e30f54f90399fdff6958392404917fdacdec2646
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 15:52:31 -0500

    Update Buffer.py

--- Buffer.py
+++ Buffer.py
@@ -1,4 +1,12 @@
-import base64,struct
+import base64,errorHandler,struct
+
+def conditionSet(var,el):
+    if (var!=None):
+        return var
+    else:
+        return el
+    ##endif
+##end
 
 def new(datstr,allowOverflows):
     class Stream:
@@ -7,7 +15,21 @@ def new(datstr,allowOverflows):
         Length=len(datstr)
         IsFinished=False
         LastUnreadBytes=0
-        AllowOverflows=conditionalSet()
-        
+        AllowOverflows=conditionalSet(allowOverflows,False)
+        def read(datlen, shift):
+            datlen=conditionalSet(datlen,1)
+            shift=conditionalSet(shift,True)
+    		dat=Stream.Source[Stream.Offset:Stream.Offset+datlen+1]
+    		dataLength=len(dat)
+    		unreadBytes=datlen-dataLength
+    		if (unreadBytes>0 and Stream.AllowOverflows!=True):
+    			errorHandler.error("Buffer went out of bounds and AllowOverflows is false")
+    		##end
+    		if (shift==True):
+    			Stream.seek(datlen)
+    		##end
+    		Stream.LastUnreadBytes=unreadBytes
+    		return dat
+        ##end
     ##end
 ##end
