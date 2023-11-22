eec64a31b15b35da201c1b4091b1e33ca6bf5c00
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 17:38:56 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-import base64,Buffer,LZ4,os,pyrbxm,sys,requests,robloxapi
+import base64,Buffer,errorHandler,LZ4,os,pyrbxm,sys,requests,robloxapi
 from pyrbxm.binary import BinaryRobloxFile
 from api import app,jsonify,getRequest
 HEADER = "<roblox!"
@@ -9,7 +9,10 @@ def parse(file):
     parsedString = ""
     with open(file, "rb") as file:
         rbxmBuffer=Buffer.new(buffer, false)
-        
+        # Read metadata / header
+        if (rbxmBuffer.read(8)!=HEADER or rbxmBuffer.read(6)!=RBXM_SIGNATURE):
+            errorHandler.error("Provided file does not match the header of an RBXM file.")
+        ##end
     ##endwith
 ##end
 def toJson(obj):
