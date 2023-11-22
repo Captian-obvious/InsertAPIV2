407d91d572b02ed17bba3ea5c095209cd8545d63
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 18:59:08 -0500

    Update basicTypes.py

--- basicTypes.py
+++ basicTypes.py
@@ -1,5 +1,4 @@
 import base64,Buffer,errorHandler,os,pyrbxm,struct,sys,requests,robloxapi
-
 def conditionalSet(condition,val1,val2):
     if ((condition)==True):
         return val1
@@ -7,17 +6,18 @@ def conditionalSet(condition,val1,val2):
         return val2
     ##endif
 ##end
-
 def lrotate(n,d):
     return (n << d)|(n >> (32 - d))
 ##end
 def rrotate(n,d):
     return (n >> d)|(n << (32 - d)) & 0xFFFFFFFF
 ##end
-
+def transformInt(x):
+    return conditionalSet(x % 2 == 0,x / 2,-(x + 1) / 2)
+##end
 def rbxF32(x):
     x=rrotate(x, 1)
-    return conditionalSet(x % 2 == 0,x / 2,-(x + 1) / 2)
+    return struct.unpack(">f", struct.pack(">I4", x))
 ##end
 def String(buffer):
     return buffer.read(buffer.readNumber("<I4"))
