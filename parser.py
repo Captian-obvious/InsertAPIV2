72096cff9275481eaa6b3240062f9bee21a78b5b
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 05:18:10 -0500

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,5 +1,8 @@
 import base64,os,pyrbxm,sys,requests,robloxapi
 from pyrbxm.binary import BinaryRobloxFile
+HEADER = "<roblox!"
+RBXM_SIGNATURE = "\x89\xff\x0d\x0a\x1a\x0a"
+ZSTD_HEADER = "\x28\xB5\x2F\xFD"
 from api import app,jsonify,getRequest
 ##class COMPILER:
 def parse(file):
