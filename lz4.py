6c8639cbdd4ac60fb434236d39a865324ef0bf0d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-11-22 17:29:03 -0500

    Update lz4.py

--- lz4.py
+++ lz4.py
@@ -30,7 +30,33 @@ def decompress(lz4data):
         outputStream.append(literal)
         outputStream.toEnd()
         if (outputStream.Length<decompressedLen):
-            offset=struct.unpack("<I2",inputStream.read(2))   
+            offset=struct.unpack("<I2",inputStream.read(2))
+            if (matLen>=19):
+                ifinish=False
+                while (ifinish!=True):
+                    nextByte=ord(inputStream.read())
+                    litLen=litLen+nextByte
+                    if (nextByte!=0xFF):
+                        ifinish=True
+                    ##endif
+                ##end
+            ##endif
+            outputStream.seek(-offset)
+            pos=outputStream.Offset
+            match=outputStream.read(matLen)
+            unreadBytes=outputStream.LastUnreadBytes
+            extra=None
+            if (unreadBytes!=0):
+                while (unreadBytes>0):
+                    outputStream.Offset=pos
+                    extra=outputStream.read(unreadBytes)
+                    unreadBytes=outputStream.LastUnreadBytes
+                    match=match+extra
+                ##end
+            ##endif
+            outputStream.append(match)
+            outputStream.toEnd()
         ##endif
     ##end
+    return outputStream.Source
 ##end
