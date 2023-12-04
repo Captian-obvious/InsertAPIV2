1ca4e1900f53f76b3c1490363357fc80d3f8016d
Anonymous <84868516+Captian-obvious@users.noreply.github.com>
2023-12-04 16:44:03 -0500

    Update tests.py

--- tests.py
+++ tests.py
@@ -2,3 +2,11 @@ from api import app
 def test():
     pass
 ##end
+def extract(byte,field,width):
+    width=conditionalSet(width!=None,width,1)
+    shifted_number=byte >> (width-1)
+    mask = (1 << width) - 1
+    extracted_bits=shifted_number & mask
+    extracted_number=bin(extracted_bits)[2:]
+    decimal_value=int(extracted_number, 2)
+##end
